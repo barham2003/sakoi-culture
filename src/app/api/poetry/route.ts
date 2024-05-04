a37dd9ee7c338d6c3328ede77dfdfe53de2275fb
baran6688 <baranali6688@gmail.com>
2024-05-04 20:33:33 +0300

    make it unstable

--- src/app/api/poetry/route.ts
+++ src/app/api/poetry/route.ts
@@ -1,12 +1,23 @@
 import { db } from "@/db";
 import { poetries } from "@/db/schema";
 import { sql } from "drizzle-orm";
+import { unstable_cache as us } from "next/cache";
 import { NextResponse } from "next/server";
 
+
+const getUnstableRandomPoetry = us(
+    () =>
+        db
+            .select()
+            .from(poetries)
+            .orderBy(sql`RANDOM()`)
+            .limit(1),
+    ["poetry"],
+    { tags: ["poetry"], revalidate: 0.1 },
+);
+
+
 export async function GET(request: Request) {
-    const quote = await db.select()
-        .from(poetries)
-        .orderBy(sql`RANDOM()`)
-        .limit(1)
-    return NextResponse.json(quote)
+    const poetry = await getUnstableRandomPoetry()
+    return NextResponse.json(poetry)
 }
--- src/app/api/quote/route.ts
+++ src/app/api/quote/route.ts
@@ -3,11 +3,17 @@ import { db } from "@/db";
 import { quotes } from "@/db/schema";
 import { sql } from "drizzle-orm";
 import { NextResponse } from "next/server";
+import { unstable_cache as us } from "next/cache";
+
+
+// *  Get Random Quote
+const getUnstableQuote = us(() => db.select()
+    .from(quotes)
+    .orderBy(sql`RANDOM()`)
+    .limit(1), ["quotes"], { tags: ["quotes"], revalidate: 0.1 })
+
 
 export async function GET(request: Request) {
-    const quote = await db.select()
-        .from(quotes)
-        .orderBy(sql`RANDOM()`)
-        .limit(1)
+    const quote = await getUnstableQuote()
     return NextResponse.json(quote)
 }
