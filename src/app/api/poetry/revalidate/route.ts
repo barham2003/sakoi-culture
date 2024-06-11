352df38d6bd6d930831798e65738cfae7c40af4b
baran6688 <baranali6688@gmail.com>
2024-06-11 17:39:15 +0300

    Letting everyone sends request

--- src/app/api/poetry/revalidate/route.ts
+++ src/app/api/poetry/revalidate/route.ts
@@ -4,5 +4,11 @@ import { NextResponse } from "next/server";
 export async function GET() {
     revalidateTag("poetry")
     revalidatePath("/poetry/list")
-    return NextResponse.json({ message: "revalidated" })
+    return NextResponse.json({ message: "revalidated" }, {
+        status: 200, headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        }
+    })
 }
