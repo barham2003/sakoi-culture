8ca4f4fc5be918a097bb70a5c62a750f09dd2c1c
baran6688 <baranali6688@gmail.com>
2024-08-02 20:50:39 +0300

    Small Change

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function notFound() {
   return (
     <h1 className=" my-64 w-full text-center text-3xl font-semibold text-destructive lg:my-40 lg:text-4xl">
-      ئەم پەڕەیە بوونی نیە
+      ببورە ئەم پەڕەیە بوونی نیە.
     </h1>
   );
 }
